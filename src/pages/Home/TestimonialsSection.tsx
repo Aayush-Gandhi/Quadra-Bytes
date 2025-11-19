2299d93cec65d82dd5fa922b668a1f9f72588034
Aayush-Gandhi <gandhiaayush21@gmail.com>
2025-11-19 11:13:09 -0500

    Merge conflit solved

--- src/pages/Home/TestimonialsSection.tsx
+++ src/pages/Home/TestimonialsSection.tsx
@@ -4,21 +4,21 @@ export default function TestimonialsSection() {
   const testimonials = [
     {
       name: "Michael Rivera",
-      // role: "CTO, Horizon Tech",
+      role: "CTO, Horizon Tech",
       message:
         "Quadra Bytes completely revamped our web platform. Their development team delivered a fast, modern, and scalable solution that improved our user experience and boosted engagement across the board.",
       // image: "/Images/Testimonials/john.jpg",
     },
     {
       name: "Sarah Thompson",
-      // role: "Marketing Director, BrightWave Media",
+      role: "Marketing Director, BrightWave Media",
       message:
         "Partnering with Quadra Bytes elevated our entire marketing strategy. Their creative approach, data-driven insights, and flawless execution helped us reach new audiences and exceed our campaign goals.",
       // image: "/Images/Testimonials/jane.jpg",
     },
     {
       name: "Tim David",
-      // role: "Founder, Nova Innovations",
+      role: "Founder, Nova Innovations",
       message:
         "Our experience with Quadra Bytes has been outstanding. Their professionalism, responsiveness, and commitment to quality set them apart. They’ve become our go-to partner for all things digital.",
       //  image: "/Images/Testimonials/alex.jpg",
